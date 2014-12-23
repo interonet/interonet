119e3d7b8e7a01f7f6b15ca7aefa487202cbf1b9
HouLifei <632023637@qq.com>
2014-12-23 22:07:49 +0800

    delete

--- ldm/src/main/java/org/interonet/ldm/vmm/createBridgeVALN.java
+++
@@ -1,9 +0,0 @@
-package com.init.bridge;
-
-import com.jcraft.jsch.JSchException;
-
-public interface createBridgeVALN {
-	String createbr() throws JSchException;
-	String createvlan() throws JSchException;
-	String addBridgeToVlan() throws JSchException;
-}
