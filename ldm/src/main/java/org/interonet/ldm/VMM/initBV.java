550d88d2dfcdca11cb2fd2d340167a070a03f7a6
Samuel Deng <mr.dengshuoling@gmail.com>
2014-12-30 23:03:04 +0800

    delete the unuseful comment auto-gened by Idea.
    Signed-off-by: Samuel Deng <mr.dengshuoling@gmail.com>

--- ldm/src/main/java/org/interonet/ldm/VMM/initBV.java
+++ ldm/src/main/java/org/interonet/ldm/VMM/initBV.java
@@ -2,9 +2,6 @@ package org.interonet.ldm.VMM;
 
 import com.jcraft.jsch.JSchException;
 
-/**
- * Created by houlifei on 14-12-25.
- */
 public interface initBV {
     String createBridge() throws JSchException;
 
