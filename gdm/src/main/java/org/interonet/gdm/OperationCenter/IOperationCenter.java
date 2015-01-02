592109b4c0af45d6dd0080be99fdf716c21ccb70
Samuel Deng <mr.dengshuoling@gmail.com>
2015-01-02 14:59:43 +0800

    extract interface in OperationCenter
    Signed-off-by: Samuel Deng <mr.dengshuoling@gmail.com>

---
+++ gdm/src/main/java/org/interonet/gdm/OperationCenter/IOperationCenter.java
@@ -0,0 +1,26 @@
+package org.interonet.gdm.OperationCenter;
+
+/**
+ * Created by samuel on 1/2/15.
+ */
+public interface IOperationCenter {
+    void createTunnelSW2SW(int switchPortPeer, int peerSwitchPortPeer) throws Throwable;
+
+    void createTunnelSW2VM(int switchPortPeeronTT, int peerVMPortPeeronTT) throws Throwable;
+
+    void addSWitchConf(Integer switchID, String controllerIP, int controllerPort) throws Throwable;
+
+    void powerOnSwitch(Integer switchID) throws Throwable;
+
+    void powerOnVM(Integer vmID) throws Throwable;
+
+    void deleteTunnelSW2SW(int switchPortPeeronTT, int athrSwitchPortPeeronTT) throws Throwable;
+
+    void deleteTunnelSW2VM(int switchPortPeeronTT, int peerVMPortPeeronTT) throws Throwable;
+
+    void deleteSWitchConf(Integer switchID) throws Throwable;
+
+    void powerOffSwitch(Integer switchID) throws Throwable;
+
+    void powerOffVM(Integer vmID) throws Throwable;
+}
--- gdm/src/main/java/org/interonet/gdm/OperationCenter/OperationCenter.java
+++ gdm/src/main/java/org/interonet/gdm/OperationCenter/OperationCenter.java
@@ -7,7 +7,7 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-public class OperationCenter {
+public class OperationCenter implements IOperationCenter {
     Map<String, String> localDomains;
     JsonRpcHttpClient client;
 
@@ -23,52 +23,62 @@ public class OperationCenter {
     }
 
 
+    @Override
     public void createTunnelSW2SW(int switchPortPeer, int peerSwitchPortPeer) throws Throwable {
 //        client.invoke("createTunnelSW2SW", new Object[]{switchPortPeer, peerSwitchPortPeer}, String.class);
         System.out.println("LDM --> createTunnelSW2SW(switchPortPeer=" + switchPortPeer + ",peerSwitchPortPeer=" + peerSwitchPortPeer + ")");
     }
 
+    @Override
     public void createTunnelSW2VM(int switchPortPeeronTT, int peerVMPortPeeronTT) throws Throwable {
 //        client.invoke("createTunnelSW2VM", new Object[]{switchPortPeeronTT, peerVMPortPeeronTT}, String.class);
         System.out.println("LDM --> createTunnelSW2VM(switchPortPeeronTT=" + switchPortPeeronTT + ",peerVMPortPeeronTT=" + peerVMPortPeeronTT + ")");
     }
 
+    @Override
     public void addSWitchConf(Integer switchID, String controllerIP, int controllerPort) throws Throwable {
 //        client.invoke("addSWitchConf", new Object[]{controllerIP, controllerPort}, String.class);
         System.out.println("LDM --> addSWitchConf(switchID=" + switchID + ",controllerIP=" + controllerIP + ",controllerPort=" + controllerPort + ")");
     }
 
+    @Override
     public void powerOnSwitch(Integer switchID) throws Throwable {
 //        client.invoke("powerOnSwitch", new Object[]{switchID}, String.class);
         System.out.println("LDM --> powerOnSwitch(switchID=" + switchID + ")");
     }
 
+    @Override
     public void powerOnVM(Integer vmID) throws Throwable {
 //        client.invoke("powerOnVM", new Object[]{vmID}, String.class);
         System.out.println("LDM --> powerOnVM(vmID=" + vmID + ")");
     }
 
+    @Override
     public void deleteTunnelSW2SW(int switchPortPeeronTT, int athrSwitchPortPeeronTT) throws Throwable {
 //        client.invoke("deleteTunnelSW2SW", new Object[]{switchPortPeeronTT, athrSwitchPortPeeronTT}, String.class);
         System.out.println("LDM --> deleteTunnelSW2SW(switchPortPeeronTT=" + switchPortPeeronTT + ",athrSwitchPortPeeronTT=" + athrSwitchPortPeeronTT + ")");
     }
 
+    @Override
     public void deleteTunnelSW2VM(int switchPortPeeronTT, int peerVMPortPeeronTT) throws Throwable {
 //        client.invoke("deleteTunnelSW2VM", new Object[]{switchPortPeeronTT, peerVMPortPeeronTT}, String.class);
         System.out.println("LDM --> deleteTunnelSW2VM(switchPortPeeronTT=" + switchPortPeeronTT + ",peerVMPortPeeronTT=" + peerVMPortPeeronTT + ")");
     }
 
+    @Override
     public void deleteSWitchConf(Integer switchID) throws Throwable {
 //        client.invoke("deleteSWitchConf", new Object[]{switchID}, String.class);
         System.out.println("LDM --> deleteSWitchConf(switchID=" + switchID + ")");
     }
 
+    @Override
     public void powerOffSwitch(Integer switchID) throws Throwable {
 //        client.invoke("powerOffSwitch", new Object[]{switchID}, String.class);
         System.out.println("LDM --> powerOffSwitch(switchID=" + switchID);
 
     }
 
+    @Override
     public void powerOffVM(Integer vmID) throws Throwable {
 //        client.invoke("powerOffVM", new Object[]{vmID}, String.class);
         System.out.println("LDM --> powerOffVM(vmID=" + vmID + ")");
