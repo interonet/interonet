cc74b307a1160abcd525174eefb1ee40c61c2ff4
Samuel Deng <mr.dengshuoling@gmail.com>
2015-03-27 23:07:56 +0800

    addSwitchConf finished for patch.
    Signed-off-by: Samuel Deng <mr.dengshuoling@gmail.com>

--- ldm/src/main/java/org/interonet/ldm/Core/LDMCore.java
+++ ldm/src/main/java/org/interonet/ldm/Core/LDMCore.java
@@ -1,8 +1,15 @@
 package org.interonet.ldm.Core;
 
+import org.interonet.ldm.ConfigurationCenter.ConfigurationCenter;
+import org.interonet.ldm.ConfigurationCenter.IConfigurationCenter;
+import org.interonet.ldm.SwitchManager.ISwitchManager;
+import org.interonet.ldm.SwitchManager.SwitchManager;
 import org.interonet.ldm.VMM.*;
 import org.libvirt.Connect;
 import org.libvirt.LibvirtException;
+
+import javax.security.auth.login.Configuration;
+import java.io.IOException;
 
 public class LDMCore {
     @SuppressWarnings("FieldCanBeLocal")
@@ -13,22 +20,29 @@ public class LDMCore {
     @SuppressWarnings("FieldCanBeLocal")
     private IBridgeAndVlan iBridgeAndVlan;
 
+    private ISwitchManager switchManager;
+
+    private IConfigurationCenter configurationCenter;
 
     public void start() {
         ldmAgent = new LDMAgent(this);
-        iCreateVirtualMachine = new CreateVirtualMachine();
-        iDeleteVirtualMachine = new DeleteVirtualMachine();
-        iBridgeAndVlan = new BridgeAndVlan();
-        iBridgeAndVlan.bridgeAndvlan();  //创建网桥和Vlan
+
+        // VMM initiation.
         try {
             connect = new Connect("qemu+tcp://400@202.117.15.94/system", false);
         } catch (LibvirtException e) {
             e.printStackTrace();
         }
+
+        // ConfigurationCenter initiation.
+        configurationCenter = new ConfigurationCenter(this);
+
+        // SwitchManager initiation.
+        switchManager = new SwitchManager(this);
     }
 
     public LDMAgent getAgent() {
-        return null;
+        return ldmAgent;
     }
 
     public String powerOnVM(Integer vmID) {
@@ -52,5 +66,13 @@ public class LDMCore {
             powerOffVMResult = "success";
         return powerOffVMResult;
 
+    }
+
+    public void addSwitchConf(Integer switchID, String controllerIP, int controllerPort) throws IOException{
+        switchManager.changeConnectionPropertyFromNFS(switchID, controllerIP, controllerPort);
+    }
+
+    public IConfigurationCenter getConfigurationCenter() {
+        return configurationCenter;
     }
 }
