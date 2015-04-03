ff5da3dff7b666ef13cc8fbeb733dc06c42823dd
Samuel Deng <mr.dengshuoling@gmail.com>
2015-04-03 19:55:19 +0800

    update jrpctester util.
    Signed-off-by: Samuel Deng <mr.dengshuoling@gmail.com>

--- util/JRPCTester/Main.java
+++ util/JRPCTester/Main.java
@@ -22,16 +22,23 @@ public class Main {
                 client = new JsonRpcHttpClient(new URL("http://202.117.15.79:8080/"));
             }
             String token = client.invoke("authenticateUser", new Object[]{username, password}, String.class);
+            if(token.equals("Failed")) {
+                System.out.print("Auth Failed");
+                System.exit(1);
+            }
             System.out.println("your token is: " + token);
+
             while (true) {
-                System.out.println("***************************");
+                System.out.println("*******************************");
                 System.out.println("1: getSwitchesUsageStatus");
                 System.out.println("2: getVMsUsageStatus");
                 System.out.println("3: getOrdersList");
                 System.out.println("4: getOrderInfoByID");
                 System.out.println("5: getRunningSlices");
                 System.out.println("6: orderSlice");
-                System.out.println("***************************");
+                System.out.println("7: deleteOrderByID");
+                System.out.println("0: exit");
+                System.out.println("*******************************");
                 int cmd = Integer.parseInt(br.readLine());
                 switch (cmd) {
                     case 1:
@@ -79,12 +86,24 @@ public class Main {
                                 + "      \"s1\" : \"custom\""
                                 + "  },"
                                 + "  \"controllerConf\": {"
-                                + "      \"ip\": \"202.117.15.123\","
+                                + "      \"ip\": \"10.0.0.253\","
                                 + "      \"port\" : \"6633\""
                                 + "  }"
                                 + "}";
                         String orderingStatus1 = client.invoke("orderSlice", new Object[]{token, order1}, String.class);
                         System.out.println("OrderingStatus:\n" + orderingStatus1);
+                        break;
+                    case 7:
+                        System.out.println("Enter orderId");
+                        String orderId = br.readLine();
+                        String status = client.invoke("deleteOrderByID", new Object[]{token, orderId}, String.class);
+                        System.out.println(status);
+                        break;
+                    case 0:
+                        System.out.println("Bye");
+                        System.exit(1);
+                    default:
+                        System.out.println("Error");
                 }
             }
         } catch (Exception e) {
