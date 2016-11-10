f8dd5aa2f6c11d764e2f3434387a9a2502066eff
侯丽菲 <houlifei@Macbook.lan>
2016-11-10 17:37:32 +0800

    start mn in vm

--- ldmFlask/app/views.py
+++ ldmFlask/app/views.py
@@ -1,33 +1,20 @@
 from app import app
 from flask import request
 import json
-# import multiprocessing
-# import time
-# from topology import initMininetTopology
-# from vertifyTopo import Vertify
+import multiprocessing
+import time
+from topology import initMininetTopology
+from vertifyTopo import Vertify
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-	return "Hello World"
-
-# @app.route('/testPost', methods=['GET','POST'])
-# def testPost():
-# 	if request.method == 'POST':
-# 		topo = json.loads(request.form['data'])
-# 		if Vertify(topo).vertify():
-# 			p = multiprocessing.Process(target=initMininetTopology,args=(topo,))
-# 			p.start()
-# 			return "success"
-# 		return 'failure'
-
-
-@app.route('/testGDM', methods=['GET','POST'])
-def testGDM():
+@app.route('/start/mn', methods=['GET','POST'])
+def start_mn():
 	if request.method == 'POST':
-		print "test"
-		topo = request.form['data']
-		print topo
-		return "success"
+		topo = json.loads(request.form['data'])
+		if Vertify(topo).vertify():
+			p = multiprocessing.Process(target=initMininetTopology,args=(topo,))
+			p.start()
+			return "success"
+		return 'failure'
+
 
